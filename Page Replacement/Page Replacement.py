c589f1b68fcaf1cba7e8958d2afa5f05a8bb3919
Estevam Amorim <estevamyusukeamorim@gmail.com>
2022-07-14 20:06:09 -0300

    add files

---
+++ Page Replacement/Page Replacement.py
@@ -0,0 +1,175 @@
+import sys
+import copy
+from collections import deque
+
+if sys.stdin.isatty(): sys.exit()
+
+input = ""
+
+for line in sys.stdin:
+    input += line
+
+def convert(input):
+    page_ref = []
+    
+    list_str = input.strip(' ').strip('\n').split('\n')
+    page_frame = int(list_str[0])
+    list_str.pop(0)
+
+    for i in list_str:
+        page_ref.append(int(i))
+    
+    return (page_ref, page_frame)
+
+def segunda_chance(pag_ref, pag_frame):
+    pag_fault = 0
+    ref_count = 0
+    memory = [[None, 0] for _ in range(pag_frame)]
+
+    circular_queue = deque()
+    
+    for i in range(len(pag_ref)):
+        success = False
+        for j in range(pag_frame):
+            if memory[j][0] == None:
+                memory[j][0] = pag_ref[i]
+                memory[j][1] = 1
+                circular_queue.append(j)
+                pag_fault += 1
+                success = True
+                break
+            
+            if memory[j][0] == pag_ref[i]:
+                memory[j][1] = 1
+                success = True
+                break
+        
+        if not success:
+            index = circular_queue[0]
+            
+            while memory[index][1] != 0:
+                circular_queue.rotate(-1)
+                index = circular_queue[0]
+            
+            circular_queue.rotate(-1)
+
+            memory[index][0] = pag_ref[i]
+            memory[index][1] = 1
+            pag_fault += 1
+
+        ref_count += 1
+        if ref_count == 4:
+            ref_count = 0
+            
+            for j in range(pag_frame):
+                memory[j][1] = 0
+
+    print('SC {}'.format(pag_fault))
+
+def algoritmo_otimo(pag_ref, pag_frame):
+    pag_fault = 0
+    memory = [[None, 0] for _ in range(pag_frame)]
+    
+    for i in range(len(pag_ref)):
+        success = False
+        for j in range(pag_frame):
+            if memory[j][0] == None:
+                memory[j][0] = pag_ref[i]
+                pag_fault += 1
+                success = True
+                break
+            
+            if memory[j][0] == pag_ref[i]:
+                success = True
+                break
+        
+        if not success:
+            last_ref = 0
+            ref_count = 0
+            
+            for j in range(pag_frame):
+                will_be_ref = False
+
+                for k in range(i, len(pag_ref)):
+                    if pag_ref[k] == memory[j][0]:
+                        will_be_ref = True
+                        break
+                    ref_count += 1
+                
+                if will_be_ref:
+                    memory[j][1] = ref_count
+                else:
+                    memory[j][1] = None
+
+            for j in range(pag_frame):
+                if memory[j][1] == None:
+                    last_ref = j
+                    break
+                if memory[j][1] > memory[last_ref][1]:
+                    last_ref = j
+
+            memory[last_ref][0] = pag_ref[i]
+            pag_fault += 1
+
+    print('OTM {}'.format(pag_fault))
+
+def conjunto_de_trabalho(pag_ref, pag_frame):
+    pag_fault = 0
+    ref_count = 0
+    virtual_time = 0
+    threshold = int(pag_frame/2) + 1
+    memory = [[None, 0, 0] for _ in range(pag_frame)]
+    
+    for i in range(len(pag_ref)):
+        virtual_time += 1
+        success = False
+        for j in range(pag_frame):
+            if memory[j][0] == None:
+                memory[j][0] = pag_ref[i]
+                memory[j][1] = 1
+                memory[j][2] = virtual_time
+                pag_fault += 1
+                success = True
+                break
+            
+            if memory[j][0] == pag_ref[i]:
+                memory[j][1] = 1
+                memory[j][2] = virtual_time
+                success = True
+                break
+        
+        if not success:
+            replaced = 0
+            
+            for j in range(pag_frame):
+                if memory[j][1] == 0:
+                    age = virtual_time - memory[j][2]
+                    if age > threshold:
+                        replaced = j
+                        break
+                    if age > (virtual_time - memory[replaced][2]):
+                        replaced = j
+                         
+            memory[replaced][0] = pag_ref[i]
+            memory[replaced][1] = 1
+            memory[replaced][2] = virtual_time
+
+            pag_fault += 1
+
+        ref_count += 1
+        if ref_count == 4:
+            ref_count = 0
+            
+            for j in range(pag_frame):
+                memory[j][1] = 0
+
+    print('CT {}'.format(pag_fault))
+
+
+pag_ref, pag_frame = convert(input)
+
+segunda_chance(copy.deepcopy(pag_ref), pag_frame)
+
+algoritmo_otimo(copy.deepcopy(pag_ref), pag_frame)
+
+conjunto_de_trabalho(copy.deepcopy(pag_ref), pag_frame)
---
+++ Process Scheduling/Process Scheduling.py
@@ -0,0 +1,207 @@
+import sys
+import random
+import copy
+from collections import deque
+
+if sys.stdin.isatty(): sys.exit()
+
+input = ""
+    
+for line in sys.stdin:
+   input += line
+
+def convert(input):
+    st = []
+    list_str = input.strip(' ').strip('\n').split('\n')
+
+    for i in list_str:
+        list_aux = i.split(' ')
+        list_int = list(map(int, list_aux))
+        st.append(list_int)
+    
+    return st
+
+def prioridades_dinamicas(st):
+    avg_return = 0
+    avg_arrival = 0
+    avg_waiting = 0
+
+    num_of_proc = len(st)
+    end_t = 0
+
+    for i in range(num_of_proc):
+        end_t += st[i][1]
+        st[i].append(5)
+        st[i].append(False)
+
+    i = 0
+
+    while i <= end_t:
+        exc = 0
+        flag = False
+        for j in range(len(st)):
+            if(st[j][0] <= i and (st[j][2] > st[exc][2] or not flag)):
+                exc = j
+                flag = True
+            st[j][2] += 1
+            if(st[j][0] <= i):
+                avg_waiting += 1
+
+        if not flag:
+            i += 1
+            continue
+        
+        st[exc][1] -= 1
+        st[exc][2] -= 2
+        avg_waiting -= 1
+
+        if not st[exc][3]:
+            st[exc][3] = True
+            avg_arrival += (i - st[exc][0])
+
+        i += 1
+
+        if st[exc][1] == 0:
+            avg_return += (i - st[exc][0])
+            st.pop(exc)
+            if len(st) == 0:
+                break
+        
+    avg_return /= num_of_proc
+    avg_arrival /= num_of_proc
+    avg_waiting /= num_of_proc
+
+    print('PRI {:.2f} {:.2f} {:.2f}'.format(avg_return, avg_arrival, avg_waiting))
+
+def loteria(st):
+    avg_return = 0
+    avg_arrival = 0
+    avg_waiting = 0
+    
+    num_of_proc = len(st)
+    end_t = 0
+    
+    for i in range(num_of_proc):
+        end_t += st[i][1]
+        st[i].append(False)
+
+    i = 0
+
+    while i <= end_t:
+        count = 0
+        flag = False
+        
+        while count < len(st)*3:
+            exc = random.randrange(0, len(st))
+            if(st[exc][0] <= i):
+                flag = True
+                break
+            count += 1
+        
+        if not flag:
+            i += 1
+            continue
+        
+        if st[exc][1] > 1:
+            exc_time = 2
+        else: 
+            exc_time = 1
+
+        st[exc][1] -= exc_time
+
+        if not st[exc][2]:
+            st[exc][2] = True
+            avg_arrival += (i - st[exc][0])
+
+        for j in range(len(st)):
+            if(j != exc):
+                if(st[j][0] <= i):
+                    avg_waiting += exc_time
+                if(exc_time == 2 and st[j][0] == i + 1):
+                    avg_waiting += 1
+
+        i += exc_time
+
+        if st[exc][1] == 0:
+            avg_return += (i - st[exc][0])
+            st.pop(exc)
+            if(len(st) == 0):
+                break
+        
+    avg_return /= num_of_proc
+    avg_arrival /= num_of_proc
+    avg_waiting /= num_of_proc
+
+    print('LOT {:.2f} {:.2f} {:.2f}'.format(avg_return, avg_arrival, avg_waiting))
+
+def round_robin(st):
+    avg_return = 0
+    avg_arrival = 0
+    avg_waiting = 0
+    
+    num_of_proc = len(st)
+    end_t = 0
+    circular_queue = deque()
+    
+    for i in range(num_of_proc):
+        end_t += st[i][1]
+        st[i].append(False)
+        circular_queue.append(st[i])
+
+    i = 0
+
+    while i <= end_t:
+        flag = False
+        for j in range(len(circular_queue)):
+            if(circular_queue[0][0] <= i):
+                flag = True
+                break
+            else:
+                circular_queue.rotate(-1)
+
+        if not flag:
+            i += 1
+            continue
+        
+        if circular_queue[0][1] > 1:
+            exc_time = 2
+        else: 
+            exc_time = 1
+
+        circular_queue[0][1] -= exc_time
+
+        if not circular_queue[0][2]:
+            circular_queue[0][2] = True
+            avg_arrival += (i - circular_queue[0][0])
+
+        for j in range(1, len(circular_queue)):
+            if(circular_queue[j][0] <= i):
+                avg_waiting += exc_time
+            if(exc_time == 2 and circular_queue[j][0] == i + 1):
+                avg_waiting += 1
+
+        i += exc_time
+
+        if circular_queue[0][1] == 0:
+            avg_return += (i - circular_queue[0][0])
+            circular_queue.rotate(-1)
+            circular_queue.pop()
+            if len(circular_queue) == 0:
+                break
+        else:
+            circular_queue.rotate(-1)
+
+    avg_return /= num_of_proc
+    avg_arrival /= num_of_proc
+    avg_waiting /= num_of_proc
+
+    print('RR {:.2f} {:.2f} {:.2f}'.format(avg_return, avg_arrival, avg_waiting))
+
+
+st_proc = convert(input)
+
+prioridades_dinamicas(copy.deepcopy(st_proc))
+
+loteria(copy.deepcopy(st_proc))
+
+round_robin(copy.deepcopy(st_proc))
